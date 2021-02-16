edb7206533c864491f360dee71128aa6f5c670d9
Fer-Romero96 <A01335834@itesm.mx>
2021-02-16 13:09:53 -0600

    Iniciamos el proyecto

---
+++ app/src/main/java/edu/itesm/justiceleague/MainActivity.kt
@@ -0,0 +1,28 @@
+package edu.itesm.justiceleague
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    data class SuperHeroe(val nombre: String, val mail: String, val movil: String, val edad: Int)
+    fun main() {
+        println("Hello, world!!!")
+
+        val batman = SuperHeroe("Bruce Wayne","batman@itesm.mx","5536776645",25)
+
+        // Tenemos que ponerle el tipo de dato en este caso SuperHeroe
+        val jl = mutableListOf<SuperHeroe>()
+
+        jl.add(batman)
+        jl.add(SuperHeroe("CK","super","77",45))
+
+        println(jl)
+
+        println(batman)
+    }
+}
